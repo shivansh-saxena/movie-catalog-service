33a4c315c6e841874f466cc44c4b03cfdbe26ed9
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-01-19 00:52:16 +0530

    returning catalog for each movie rating by invoking a endpoint using rest template Making Rest Template @Bean in main class for initializing it only once

--- src/main/java/com/shiv/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/shiv/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,10 +1,13 @@
 package com.shiv.moviecatalogservice.resources;
 
 import com.shiv.moviecatalogservice.models.CatalogItem;
+import com.shiv.moviecatalogservice.models.Movie;
 import com.shiv.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -15,6 +18,8 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
 
@@ -22,7 +27,10 @@ public class MovieCatalogResource {
                 new Rating("mov 1",4),
                 new Rating("mov 2",3)
         );
-        return ratings.stream().map( rating -> new CatalogItem("Transformers","part 1", 4)).
+        return ratings.stream().map( rating -> {
+                    Movie movie = restTemplate.getForObject("http://localhost:9002/movies/id", Movie.class);
+                    return new CatalogItem(movie.getName(),"part 1", rating.getRating());
+                }).
                 collect(Collectors.toList());
 
 //        return Collections.singletonList(
