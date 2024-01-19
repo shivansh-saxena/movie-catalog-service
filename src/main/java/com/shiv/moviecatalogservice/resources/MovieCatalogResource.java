702574d30def501c6e51e8262fed312f314236c6
Shivansh Saxena <shivanshsaxena08@gmail.com>
2024-01-19 21:41:41 +0530

    Making the call using Web Client instead of Rest Template(it has async logic)

--- src/main/java/com/shiv/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/com/shiv/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -20,15 +21,23 @@ public class MovieCatalogResource {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder builder;
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId){
-
         List<Rating> ratings = Arrays.asList(
                 new Rating("mov 1",4),
                 new Rating("mov 2",3)
         );
         return ratings.stream().map( rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:9002/movies/id", Movie.class);
+//                    Movie movie = restTemplate.getForObject("http://localhost:9002/movies/id", Movie.class);
+                    Movie movie = builder.build()
+                            .get()
+                            .uri("http://localhost:9002/movies/id")
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
                     return new CatalogItem(movie.getName(),"part 1", rating.getRating());
                 }).
                 collect(Collectors.toList());
